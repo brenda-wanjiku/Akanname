704d6448b1d90f28e21e9f2b27b5e7b6b60d4329
brenda-wanjiku <brendawanjiku@live.com>
2020-02-16 20:56:26 +0300

    Makes changes to Js

--- index.js
+++ index.js
@@ -7,19 +7,36 @@ function myFunction() {
     var DD = document.getElementById("birth-day").value; // get the element with id = "birth-day"
     var MM = document.getElementById("birth-month").value; //get the element with id = "birth-month"
 
+    form.addEventListener ('submit', (e) => {
+        let messages = []
+        if (DD <=0 || DD <31){
+            alert("Invalid Date")
+    } 
+        if (MM <=0 || MM >12){
+            alert("Invalid Month")
+    }
+        e.preventDefault()
+        errrorElement.innerText = messages.join (',')
+    })
+    
+
+
+
     var n = parseInt((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7);
   
     var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
     var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-   if (document.getElementById("male").value === "male") {
-        alert("Your Akan name is " + maleName[n] + " you were born on " + days[n]);
+    if (document.getElementById("female").value === "female") {
+        alert("Your Akan name is " + femaleName[n] + " you were born on " + days[n]);
     }
-    else if (document.getElementById("female").value === "female") {
-        alert("Your Akan name is " + femaleName[n] + " you were born on " + days[n]); 
+    else if (document.getElementById("male").value === "male") {
+        alert("Your Akan name is " + maleName[n] + " you were born on " + days[n]);
     }
     else {
         alert("Invalid")      
     }
+
+    
 }
