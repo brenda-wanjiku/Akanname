d293a7151af0db529e38c5dd116d94d6d6d2a8d6
brenda-wanjiku <brendawanjiku@live.com>
2020-02-16 15:48:33 +0300

    Adds new font link to html

--- index.js
+++ index.js
@@ -7,19 +7,6 @@ function myFunction() {
     var DD = document.getElementById("birth-day").value; // get the element with id = "birth-day"
     var MM = document.getElementById("birth-month").value; //get the element with id = "birth-month"
 
-
-    form.addEventListener("submit", (e)=> {
-        let messages = []
-        if (DD.value <=0 || DD.value > 31) {
-            messages.push("Invalid date");
-        }
-        if (MM.value <= 0 || MM.value >12) {
-            messages.push("Invalid month");
-        }
-        e.preventDefault()
-        errrorElement.innerText = messages.join(', ');
-    })
-
     var n = parseInt((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7);
 
     var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
@@ -29,12 +16,10 @@ function myFunction() {
    if (document.getElementById("male").value === "Male") {
         alert("Your Akan name is " + maleName[n] + " was born on " + days[n]);
     }
-    else if (document.getElementById("female").value === "Female") {
+    else (document.getElementById("female").value === "Female") {
         alert("Your Akan name is " + femaleName[n] + " was born on " + days[n]); 
     }
-    else {
-        alert("Invalid data");
-    }
+
 
     function check() {
         document.getElementById("female").checked = true;
