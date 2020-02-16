02f403eabe44f662ca45541679ae1657296cd5a8
brenda-wanjiku <brendawanjiku@live.com>
2020-02-16 13:24:59 +0300

    Changes functions in js. Adds new scripts.

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-function myFunction(){
+function weekday () {
 
-    var year=document.getElementById("year").value;
-    var CC=parseInt(year.slice(0,2));
-    var YY=parseInt(year.slice(2,4));
-    var DD = document.getElementById("birth-day").value; // get the element with id = "birth-day"
-    var MM = document.getElementById("birth-month").value; //get the element with id = "birth-month"
-
-    var n = parseInt(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
-    alert(n);
-
-    var maleName = ["Kwasi","Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi","Kwame"]
-    var femaleName = ["Akosua","Adwoa","Abenaa", "Akua","Yaa","Afua",
-        "Ama"]
-    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-
-    if (document.getElementById("female").value==="Female"){
-        alert("Female");
-    }
-}
-
-
-
-
-
-
+  var year = document.getElementById("year").value;
+  var CC = parseInt(year.slice(0, 2));
+  var YY = parseInt(year.slice(2, 4));
+  var DD = document.getElementById("birth-day").value; // get the element with id = "birth-day"
+  var MM = document.getElementById("birth-month").value; //get the element with id = "birth-month"
   
-    
+    if (MM === 1 || MM === 2) {
+      MM += 12;
+    } else{ 
+        MM = MM;
+    }
+
+    var n =  parseInt((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7);
+   alert (n);
+    }
