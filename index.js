7506acf9dfe061aeb9bfdf0cb19c2ba11b73310b
brenda-wanjiku <brendawanjiku@live.com>
2020-02-17 04:56:26 +0300

    Adds swal command in Js

--- index.js
+++ index.js
@@ -6,22 +6,8 @@ function myFunction() {
     var YY = parseInt(year.slice(2, 4));
     var DD = document.getElementById("birth-day").value; // get the element with id = "birth-day"
     var MM = document.getElementById("birth-month").value; //get the element with id = "birth-month"
-
-    form.addEventListener ('submit', (e) => {
-        let messages = []
-        if (DD <=0 || DD <31){
-            alert("Invalid Date")
-    } 
-        if (MM <=0 || MM >12){
-            alert("Invalid Month")
-    }
-        e.preventDefault()
-        errrorElement.innerText = messages.join (',')
-    })
+    import swal from 'sweetalert'; 
     
-
-
-
     var n = parseInt((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7);
   
     var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
@@ -29,14 +15,21 @@ function myFunction() {
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
     if (document.getElementById("female").value === "female") {
-        alert("Your Akan name is " + femaleName[n] + " you were born on " + days[n]);
+        swal("Your Akan name is " + femaleName[n] + " you were born on " + days[n]);
     }
     else if (document.getElementById("male").value === "male") {
-        alert("Your Akan name is " + maleName[n] + " you were born on " + days[n]);
+        swal("Your Akan name is " + maleName[n] + " you were born on " + days[n]);
     }
     else {
-        alert("Invalid")      
+        swal("Invalid")      
     }
+        if (DD <=0 || DD >31){
+            alert("Invalid Date");
+    } 
+        if (MM <=0 || MM >12){
+            alert("Invalid Month");
+    }
+
 
     
 }
